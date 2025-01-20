0798d37c51eeebcfa1e12b83f0ad99d47e2f0ee2
Vlamik <vladino.mikullas@gmail.com>
2025-01-20 21:23:11 +0100

    Added core architecture components

---
+++ core/commons/src/main/java/com/vlamik/core/commons/Annotations.kt
@@ -0,0 +1,12 @@
+// ktlint-disable filename
+@file:Suppress("MatchingDeclarationName", "Filename")
+
+package com.vlamik.core.commons
+
+@javax.inject.Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class BackgroundDispatcher
+
+@javax.inject.Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class ApiUrl
